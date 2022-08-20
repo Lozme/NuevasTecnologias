dd04ba41b9882f11ec5e848cebece43d3c8431f8
Mauro Lozano <lznmauricio@gmail.com>
2022-08-20 16:52:14 -0500

    Agregar import de la vista de prueba

--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 #Se debe importar la ruta de la vista
-from PaginaWeb.views import PaginaPrincipal
+from PaginaWeb.views import PaginaPrincipal,nombreVista
 
 #Se debe agregar la ruta en la que se mostrará la vista
 urlpatterns = [
