61ee4f63e6ead316f614c57989b55f7877c45595
Mauro Lozano <lznmauricio@gmail.com>
2022-08-20 16:51:54 -0500

    Agregar ruta principal

--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 #Se debe importar la ruta de la vista
-from PaginaWeb.views import nombreVista
+from PaginaWeb.views import PaginaPrincipal
 
 #Se debe agregar la ruta en la que se mostrará la vista
 urlpatterns = [
     path('NombreRuta/', nombreVista),
+    path('', PaginaPrincipal),
 ]
