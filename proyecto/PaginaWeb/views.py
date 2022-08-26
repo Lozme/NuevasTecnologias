d7f93732dac172db98227e5ef30c679f158e7330
Mauro Lozano <lznmauricio@gmail.com>
2022-08-25 22:08:25 -0500

    Se actualizan las vistas

--- proyecto/PaginaWeb/views.py
+++ proyecto/PaginaWeb/views.py
@@ -56,10 +56,10 @@ def PaginaPrincipal(response):
                                 <a class="nav-link" href="Noticias" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Noticias</a>
                                 </li>
                                 <li class="nav-item">
-                                <a class="nav-link" href="#" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Análisis</a>
+                                <a class="nav-link" href="Analisis" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Análisis</a>
                                 </li>
                                 <li class="nav-item">
-                                <a class="nav-link" href="#" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Guías</a>
+                                <a class="nav-link" href="Guias" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Guías</a>
                                 </li>
                                 <li class="nav-item">
                                 <a class="nav-link" href="Foro" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Foro</a>
@@ -314,10 +314,10 @@ def Noticias(response):
                             <a class="nav-link" href="#" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Noticias</a>
                             </li>
                             <li class="nav-item">
-                            <a class="nav-link" href="#" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Análisis</a>
+                            <a class="nav-link" href="../Analisis" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Análisis</a>
                             </li>
                             <li class="nav-item">
-                            <a class="nav-link" href="#" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Guías</a>
+                            <a class="nav-link" href="../Guias" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Guías</a>
                             </li>
                             <li class="nav-item">
                             <a class="nav-link" href="../Foro" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Foro</a>
@@ -487,7 +487,7 @@ def Foro(response):
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Página Principal</title>
+            <title>Foro</title>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
             <style>
@@ -517,10 +517,10 @@ def Foro(response):
                                 <a class="nav-link" href="../Noticias" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Noticias</a>
                                 </li>
                                 <li class="nav-item">
-                                <a class="nav-link" href="#" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Análisis</a>
+                                <a class="nav-link" href="../Analisis" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Análisis</a>
                                 </li>
                                 <li class="nav-item">
-                                <a class="nav-link" href="#" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Guías</a>
+                                <a class="nav-link" href="../Guias" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Guías</a>
                                 </li>
                                 <li class="nav-item">
                                 <a class="nav-link active" href="#" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Foro</a>
@@ -720,6 +720,201 @@ def Foro(response):
                         </ul>
                         </footer>
                     </div>
+                </div>
+            </div>
+        </body>
+        </html>
+    ''')
+
+#########################################################################################
+#########################################################################################
+
+#CREADO POR: Estiven Zapata
+def GuiaGOW(response):
+    return HttpResponse('''
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Guías</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+            <style>
+                *{ margin: 0; padding: 0; box-sizing: border-box; }
+                .row {
+                    margin-right: auto;
+                    margin-left: auto;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="row">
+                <div class="rounded-3" style="background: rgb(0,0,0);background: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(61,0,0,1) 35%);;color:aliceblue;font-size: 1.6rem;">
+                <h1 class="text-center  mt-2" style="font-size: 2.5rem;">Nombre Página / Logo</h1>
+                <p class="text-center" style="font-size: 1.5rem;">Alguna frase</p>
+                <hr style="margin: 0;">
+                <nav class="navbar navbar-expand-lg">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="../" style="color: aliceblue;font-size: 2rem;">Logo</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                <a class="nav-link" href="../Noticias" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Noticias</a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link" href="../Analisis" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Análisis</a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link" href="#" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Guías</a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link active" href="../Foro" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Foro</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                </div>
+            </div> 
+            <div class="row d-flex justify-content-center">
+                <div class="container-fluid w-75">
+                    <div>
+                        <h2 class="mt-5 text-center" style="background-color:black;color:aliceblue;">Guía God of War (PC, PS4 y PS5): Trucos, consejos y secretos</h2>
+                        <h5 class="mt-3 text-center">Enseña a Kratos a ser padre y a Atreus a ser un dios con la guía más completa de God of War en español.</h5>
+                    </div>
+                    <div class="mt-3 text-center">
+                        <img style="width:70%" src="https://media.vandal.net/m/3-2020/20203218232519_1.jpg" alt="" srcset="">
+                    </div>
+                    <div class="mt-3">
+                        <p style="text-align:justify;">
+                            Bienvenidos a la guía más completa de God of War para PS4, PC y PS5. Descubre todos los secretos del juego, complétalo al 100%, consigue todos los trofeos y descúbrelo todo sobre la nueva aventura del dios de la guerra. Como es sabido, Kratos acabó con todo aquello que se interponía en su camino en la antigua Grecia. Sin embargo, ahora, cansado y muy cambiado, vive en los lejanos reinos del norte acompañado de su hijo Atreus. No te dejes nada en God of War PS4 con nuestra guía completa del juego.
+                        </p>
+                    </div>
+                    <div>
+                        <h4>Preguntas frecuentes</h4>
+                        <p>
+                            <ul> <li style="text-align:justify;"> <b>Cómo mejorar las armaduras:</b> Tendrás que esperar a haber avanzado lo suficiente en la historia para desbloquear la tienda. Puedes mejorar tu equipo en cualquiera de las tiendas del juego.</li>
+                            </ul>
+                            <ul> <li style="text-align:justify;"> <b>Cómo conseguir los materiales para mejorar las armas:</b> Se trata de materiales que el juego nos da automáticamente al derrotar a los jefes. Para la última mejora de las armas principales debes completar los Reinos opcionales.</li>
+                            </ul>
+                            <ul> <li style="text-align:justify;"> <b>Cómo conseguir mejores armaduras:</b> Las mejores armaduras se consiguen en Niflheim. En los cofres el equipo es la mayoría de veces aleatorio.</li>
+                            </ul>
+                            <ul> <li style="text-align:justify;"> <b>Cómo conseguir XP y Plata rápidamente;</b> No hay un sistema rápido, el juego está perfectamente equilibrado en este sentido.</li>
+                            </ul>
+                            <ul> <li style="text-align:justify;"> <b>¿Hace falta haber jugado a los God of War anteriores para entender la historia?</b> No. Siempre que sepas quién es Kratos y a quiénes mató en Grecia, podrás comprender la historia perfectamente.</li>
+                            </ul>
+                        </p>
+                    </div>
+                    <div>
+                        <h4>Historia</h4>
+                        <p style="text-align:justify;">
+                            Te contamos cómo superar todas las misiones de historia de God of War, sin spoilers e indicando una buena parte de los coleccionables que encontrarás en tu camino. Si quieres exprimir tu partida al máximo, no te pierdas nuestra guía completa de la historia de God of War.
+                        </p>
+                    </div>
+                    <div>
+                        <h4>Favores</h4>
+                        <p style="text-align:justify;">
+                            God of War nos ofrece unas cuantas misiones secundarias, que en este caso llevan el nombre de Favores. Desde las criaturas más pequeñas hasta los monstruos más grandes, todos tienen algo que pedirte y nosotros te contamos cómo cumplir con todo. Todos los favores.
+                        </p>
+                    </div>
+                    <div>
+                        <h4>Valquirias</h4>
+                        <p style="text-align:justify;">
+                            Las valquirias están de nuevo sobre la tierra. Sin embargo, parece que no lo están precisamente por voluntad propia. Kratos tendrá que enfrentarse a ellas y averiguar qué ha sucedido si quiere arreglar esta situación. Derrotar a todas las valquirias no será fácil, pero nosotros te ayudaremos a salir con vida de los enfrentamientos.
+                        </p>
+                    </div>
+                    <div>
+                        <h4>Mapas del tesoro</h4>
+                        <p style="text-align:justify;">
+                            Un sitio tan antiguo como Midgard no puede tener todo a simple vista. Repartidas por las distintas regiones del Reino de los hombres, encontrarás una serie de ubicaciones que tienen un tesoro oculto. Estos tesoros contienen Recursos especiales, enfocados a poder mejorar nuestros accesorios y, cómo no, conseguir una buena cantidad de Plata.
+                            <br><br>
+                            No vayas demasiado rápido, porque tendrás que encontrar los mapas para poder coger los tesoros. Lo primero es fácil, para lo segundo puede que tengas problemas, ya que sólo te dan una imagen y una pista de dónde buscar.
+                        </p>
+                    </div>
+                    <div>
+                        <h4>Cámaras Ocultas de Odín</h4>
+                        <p style="text-align:justify;">
+                            El padre de los dioses del norte tiene una serie de escondrijos para sus tesoros repartidos por los Reinos. ¿Qué es lo que realmente ha ocultado en estos lugares y por qué? Sólo hay una manera de averiguarlo: abriendo las Cámaras. Todas las cámaras ocultas de Odín.
+                        </p>
+                    </div>                    
+                </div>
+            </div>
+        </body>
+        </html>
+    ''')
+
+#########################################################################################
+#########################################################################################
+
+#CREADO POR: Andrés Murillo
+def GuiaFreeFire(response):
+    return HttpResponse('''
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Guías</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+            <style>
+                *{ margin: 0; padding: 0; box-sizing: border-box; }
+                .row {
+                    margin-right: auto;
+                    margin-left: auto;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="row">
+                <div class="rounded-3" style="background: rgb(0,0,0);background: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(61,0,0,1) 35%);;color:aliceblue;font-size: 1.6rem;">
+                <h1 class="text-center  mt-2" style="font-size: 2.5rem;">Nombre Página / Logo</h1>
+                <p class="text-center" style="font-size: 1.5rem;">Alguna frase</p>
+                <hr style="margin: 0;">
+                <nav class="navbar navbar-expand-lg">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="../" style="color: aliceblue;font-size: 2rem;">Logo</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                <a class="nav-link" href="../Noticias" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Noticias</a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link" href="#" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Análisis</a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link" href="../Guias" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Guías</a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link active" href="../Foro" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Foro</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                </div>
+            </div> 
+            <div class="row d-flex justify-content-center">
+                <div class="container-fluid w-75">
+                    <div>
+                        <h2 class="mt-3 text-center" style="background-color:black;color:aliceblue;">Guía Free Fire, trucos, consejos y secretos</h2>
+                        <p class="mt-3">En nuestra completa guía de Free Fire, el popular juego de móviles, te enseñamos los mejores consejos para sobrevivir y ganar en sus intensas y divertidas partidas Battle Royale.</p>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <iframe class="mt-3" width="75%" height="380" src="https://www.youtube.com/embed/CgqrHG_3i9g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                    <div class="mt-3">
+                        <p style="text-align:justify;">
+                            Bienvenidos a nuestra guía completa de Free Fire, uno de los juegos móviles que más descargas acumulan en el mercado, tanto en dispositivos Android como en iOS de Apple. Garena nos brinda una experiencia de acción en tercera persona al más puro estilo Battle Royale en la que debemos caer en una isla con 50 jugadores y sobrevivir hasta el final... solo si somos capaces. Para ayudaros a brillar en el campo de batalla os traemos en nuestra guía un montón de consejos, trucos y recomendaciones de todo tipo. Aquí podréis encontrar información acerca del arsenal del juego, cómo conseguir diamantes, desbloquear contenidos y mucho más. ¡Rompan filas, soldados!
+                        </p>
+                    </div>             
                 </div>
             </div>
         </body>
