344cc58c123563fb79bf2781d3778ffedd74b2ca
Mauro Lozano <lznmauricio@gmail.com>
2022-08-25 20:23:07 -0500

    Añado ruta

--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 #Se debe importar la ruta de la vista
-from PaginaWeb.views import PaginaPrincipal,nombreVista,noticias
+from PaginaWeb.views import PaginaPrincipal,Noticias,Foro
 
 #Se debe agregar la ruta en la que se mostrará la vista
 urlpatterns = [
-    path('NombreRuta/', nombreVista),
     path('', PaginaPrincipal),
-    path('Noticias/',noticias)
+    path('Noticias/',Noticias),
+    path('Foro/',Foro)
 ]
