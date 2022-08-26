85b10874d95c600e3f18ccfa4ca36a1c0e7cf7af
camilo cano <juancamilocp30@gmail.com>
2022-08-25 19:36:06 -0500

    primera muestra

--- proyecto/PaginaWeb/views.py
+++ proyecto/PaginaWeb/views.py
@@ -152,3 +152,303 @@ def PaginaPrincipal(response):
         </body>
         </html>
     ''')
+
+
+
+
+
+
+
+
+
+
+
+def Foro(response):
+    return HttpResponse('''
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Página Principal</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+            <style>
+                *{ margin: 0; padding: 0; box-sizing: border-box; }
+                .row {
+                    margin-right: auto;
+                    margin-left: auto;
+                }
+            </style>
+            <script src="https://kit.fontawesome.com/638f505c64.js" crossorigin="anonymous"></script>
+        </head>
+        <body style=" background: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ02qYYeqeXEnbgAN_I4-AaDzDx0btqxvzW7g&usqp=CAU);">
+            <div class="row">
+                <div class="rounded-3" style="background: rgb(0,0,0);background: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(61,0,0,1) 35%);;color:aliceblue;font-size: 1.6rem;">
+                <h1 class="text-center  mt-2" style="font-size: 2.5rem;">Foros </h1>
+                <p class="text-center" style="font-size: 1.5rem;">Categoria: juegos</p>
+                <hr style="margin: 0;">
+                <nav class="navbar navbar-expand-lg">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#" style="color: aliceblue;font-size: 2rem;">Logo</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                <a class="nav-link" href="#" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Noticias</a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link" href="#" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Análisis</a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link" href="#" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Guías</a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link active" href="#" style="color: aliceblue;font-size: 1.5rem;margin-left: 5vw;">Foro</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                </div>
+            </div> 
+        
+            <div class="col-8  row d-flex justify-content-center"style="background-color: #fef7f7;">
+
+                <div class="row">
+                    <div class="col-6 mt-2">
+                        <h4 class="text-right"style="background-color: #000000;border-radius: 20px;color: aliceblue;"><p class="titxex1" style="margin-left: 5%">videojuegos</p></h4>
+ 
+                    </div>
+                    <div class="col-6 mt-2" >
+                        <h4 class="text-right" style="background-color: #000000;border-radius: 20px;color: aliceblue;"><p class="titxex1" style="margin-left: 5%">ultimos posts</p></h4>
+
+                    </div>
+                </div>
+
+
+
+
+                <div class="row">
+                    <div class="col-6 mt-2">
+
+                        <div class="container-fluid d-flex justify-content-start mt-3">
+                            <img src="https://cdn-icons-png.flaticon.com/128/8286/8286658.png" class="img-fluid" style="width: 15%;border-radius: 20px;" alt="Sekiro">                
+                            <div class="container-fluid d-flex flex-column">
+                                 <h6><a href="/" style="color: #000000; text-decoration: none;" >Videojuegos en general</a></h6><p>Temas sobre juegos en general. Lanzamientos, actualidad, gustos, preguntas...</p>
+                            </div>
+                        </div>  
+                    </div>
+                    <div class="col-6 mt-2">
+                        <div class="container-fluid d-flex justify-content-start mt-3">
+                                                
+                            <div class="container-fluid d-flex flex-column">
+                                <h6><a href="/" style="color: #000000; text-decoration: none;" >9 minutos - Cyber_Turbo_R</a></h6>
+                                <p>[Post oficial V.4] CAPTURAS DE JUEGOS</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 mt-2">
+                        <div class="container-fluid d-flex justify-content-start mt-3">
+                            <img src="https://cdn-icons-png.flaticon.com/128/189/189533.png" class="img-fluid" style="width: 15%;border-radius: 20px;" alt="Sekiro">                
+                            <div class="container-fluid d-flex flex-column">
+                                <h6> <a href="/" style="color: #000000; text-decoration: none;" >Videojuegos específicos</a></h6><p>Encuentra el foro de tu juego preferido y relaciónate con su comunidad.</p>
+                            </div>
+                        </div>  
+                    </div>
+                    <div class="col-6 mt-2">
+                        <div class="container-fluid d-flex justify-content-start mt-3">
+                                                
+                            <div class="container-fluid d-flex flex-column">
+                                <h6><a href="/" style="color: #000000; text-decoration: none;" >un momento - edurance</a></h6><p> Noticia: PlayStation se enfrenta a una demanda millonaria por una presunta 'estafa' al consumidor </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+
+                <div class="row">
+                    <div class="col-6 mt-2">
+                        <div class="container-fluid d-flex justify-content-start mt-3">
+                            <img src="https://cdn-icons-png.flaticon.com/128/2005/2005003.png" class="img-fluid" style="width: 15%;border-radius: 2px;" alt="Sekiro">                
+                            <div class="container-fluid d-flex flex-column">
+                                <h6><a href="/" style="color: #000000; text-decoration: none;" >Noticias y actualidad</a></h6><p>Tribuna abierta para comentar las noticias del mundo del videojuego.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 mt-2">
+                        <div class="container-fluid d-flex justify-content-start mt-3">
+                            <div class="container-fluid d-flex flex-column">
+                                <h6><a href="/" style="color: #000000; text-decoration: none;" >13 minutos - LordDagak</a></h6><p>● Sonic  Post oficial ●  Sonic Frontiers estas Navidades</p>
+                            </div>               
+
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div class="row">
+                    <div class="col-6 mt-2">
+                        <h4 class="text-right"style="background-color: #000000;border-radius: 20px;color: aliceblue;"><p class="titxex1" style="margin-left: 5%">MÁS QUE VIDEOJUEGOS</p></h4>
+ 
+                    </div>
+                    <div class="col-6 mt-2" >
+                        <h4 class="text-right" style="background-color: #000000;border-radius: 20px;color: aliceblue;"><p class="titxex1" style="margin-left: 5%">ultimos posts</p></h4>
+
+                    </div>
+                </div>
+
+
+
+
+
+
+
+                <div class="row">
+                    <div class="col-6 mt-2">
+                        <div class="container-fluid d-flex justify-content-start mt-3">
+                            <img src="https://cdn-icons-png.flaticon.com/128/7509/7509773.png" class="img-fluid" style="width: 15%;border-radius: 2px;" alt="Sekiro">                
+                            <div class="container-fluid d-flex flex-column">
+                                <h6><a href="/" style="color: #000000; text-decoration: none;" >Off Topic y humor</a></h6><p>Hablemos de todo menos de juegos o temas de otros foros.</p>
+                            </div>
+                        </div>  
+                    </div>
+                    <div class="col-6 mt-2">
+                        <div class="container-fluid d-flex justify-content-start mt-3">
+                            <div class="container-fluid d-flex flex-column">
+                                <h6><a href="/" style="color: #000000; text-decoration: none;" >4 horas - Jocs_204</a></h6><p> GRAN TURISMO 7  Encuentra tu trazada</p>
+                            </div>                  
+ 
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="row">
+                    <div class="col-6 mt-2">
+                        <div class="container-fluid d-flex justify-content-start mt-3">
+                            <img src="https://cdn-icons-png.flaticon.com/128/3959/3959335.png" class="img-fluid" style="width: 15%;border-radius: 2px;" alt="Sekiro">                
+                            <div class="container-fluid d-flex flex-column">
+                                <h6><a href="/" style="color: #000000; text-decoration: none;" >Desarrollo de videojuegos</a></h6><p>Comparte proyectos y conocimientos.</p>
+                            </div>
+                        </div>  
+                    </div>
+                    <div class="col-6 mt-2">
+                        <div class="container-fluid d-flex justify-content-start mt-3">
+                            <div class="container-fluid d-flex flex-column">
+                                <h6><a href="/" style="color: #000000; text-decoration: none;" >13/06/2022 17:03 - Alendi93</a></h6><p>Discord para aprender Unity y desarrollo de juegos en general [Proyecto serio]</p>
+                            </div>                  
+ 
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+
+
+
+
+
+
+                <div class="row">
+                    <div class="col-6 mt-2">
+                        <h4 class="text-right"style="background-color: #000000;border-radius: 20px;color: aliceblue;"><p class="titxex1" style="margin-left: 5%">SISTEMAS</p></h4>
+ 
+                    </div>
+                    <div class="col-6 mt-2" >
+                        <h4 class="text-right" style="background-color: #000000;border-radius: 20px;color: aliceblue;"><p class="titxex1" style="margin-left: 5%">ultimos posts</p></h4>
+
+                    </div>
+                </div>
+
+
+
+
+
+                <div class="row">
+                    <div class="col-6 mt-2">
+                        <div class="container-fluid d-flex justify-content-start mt-3">
+                            <img src="https://cdn-icons-png.flaticon.com/128/2331/2331858.png" class="img-fluid" style="width: 15%;border-radius: 40px;" alt="Sekiro">                
+                            <div class="container-fluid d-flex flex-column">
+                                <h6><a href="/" style="color: #000000; text-decoration: none;" >	PC / Hardware y presupuestos</a></h6><p>Jugadores de PC, Steam, juegos digitales y expertos en configurar equipos y presupuestos.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 mt-2">
+                        <div class="container-fluid d-flex justify-content-start mt-3">
+                            <div class="container-fluid d-flex flex-column">
+                                <h6><a href="/" style="color: #000000; text-decoration: none;" >19/08/2022 10:39 - Guthwulf</a></h6><p>Monster boy and the cursed kingdom: posible bug</p>
+                            </div>             
+
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 mt-2">
+                        <div class="container-fluid d-flex justify-content-start mt-3">
+                            <img src="https://cdn-icons-png.flaticon.com/128/1724/1724566.png" class="img-fluid" style="width: 15%;border-radius: 2px;" alt="Sekiro">                
+                            <div class="container-fluid d-flex flex-column">
+                                <h6><a href="/" style="color: #000000; text-decoration: none;" >	PlayStation 5 / PlayStation 4 / PlayStation 3</a></h6><p>Videojuegos, PSN, PSPlus, PlayStation VR y más para los seguidores de consolas Sony</p>
+                            </div>
+                        </div>  
+                    </div>
+                    <div class="col-6 mt-2">
+                        <div class="container-fluid d-flex justify-content-start mt-3">
+                            <div class="container-fluid d-flex flex-column">
+                                <h6><a href="/" style="color: #000000; text-decoration: none;" >45 minutos - Safenix</a></h6><p>[Xenohilo Oficial] Saga Xenoblade Chronicles: Monolith does what Retrodont</p>
+                            </div>                 
+ 
+                        </div>
+                    </div>
+
+                </div>
+                <div class="row">
+                    <div class="col-6 mt-2">
+                        <div class="container-fluid d-flex justify-content-start mt-3">
+                            <img src="https://cdn-icons-png.flaticon.com/128/588/588298.png" style="width: 15%;border-radius: 20px;" alt="Sekiro">                
+                            <div class="container-fluid d-flex flex-column">
+                                <h6><a href="/" style="color: #000000; text-decoration: none;" > Xbox Series X / Xbox One / Xbox 360</a></h6><p>Juegos, Xbox Live, Arcade, retrocompatibilidad y todo sobre las consolas Xbox de Microsoft.</p>
+                            </div>
+                        </div>  
+                    </div>
+                    <div class="col-6 mt-2">
+                        <div class="container-fluid d-flex justify-content-start mt-3">
+                            <div class="container-fluid d-flex flex-column">
+                                <h6><a href="/" style="color: #000000; text-decoration: none;" >21/06/2021 16:51 - Xilos</a></h6><p>P.O THE LEGEND OF SAGA [Every game has a story, Only one is a legend]b</p>
+                            </div>                 
+ 
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="row mt-4">
+                <div class="container-fluid" style="background-color: #191A19;">
+                    <footer class="d-flex flex-wrap justify-content-between align-items-center">
+                    <div class="col-md-4 d-flex align-items-center ms-5">
+                        <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                            <i style="color: aliceblue; font-size: 2.5rem;" class="fa-brands fa-bootstrap"></i>
+                         </a>
+                        <span style="font-size: 1.5rem;" class="mb-3 mb-md-0 text-muted"><p style="color: aliceblue;" class="mt-3">&copy; 2022 Nombre Página, Inc</p></span>
+                    </div>      
+                        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex me-5">
+                            <li class="ms-3"><a class="text-muted" href="#"><i style="color: aliceblue; font-size: 2.5rem;" class="fa-brands fa-twitter"></i></a></li>
+                            <li class="ms-3"><a class="text-muted" href="#"><i style="color: aliceblue; font-size: 2.5rem;" class="fa-brands fa-instagram"></i></a></li>
+                            <li class="ms-3"><a class="text-muted" href="#"><i style="color: aliceblue; font-size: 2.5rem;" class="fa-brands fa-facebook-f"></i></a></li>
+                        </ul>
+                        </footer>
+                    </div>
+                </div>
+            </div>
+        </body>
+        </html>
+    ''')
--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 #Se debe importar la ruta de la vista
-from PaginaWeb.views import PaginaPrincipal,nombreVista
+from PaginaWeb.views import PaginaPrincipal,nombreVista,Foro
 
 #Se debe agregar la ruta en la que se mostrará la vista
 urlpatterns = [
     path('NombreRuta/', nombreVista),
     path('', PaginaPrincipal),
+    path('Foro/', Foro),
+    
 ]
